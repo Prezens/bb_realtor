c5a963aff4d70c7c81e3eee18d6ff7a1247f6af0
Prezens <bunin.zhenya.99@gmail.com>
2019-04-06 22:24:27 +0600

    Added django-heroku

--- buyhome/settings.py
+++ buyhome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,5 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'bunin.zhenya.99@gmail.com'
 EMAIL_HOST_PASSWORD = '4552.E=mc^2.3.1415'
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
