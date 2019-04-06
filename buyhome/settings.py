b1fdfdcd990a3df53f6e3468fe12270ce118727f
Prezens <bunin.zhenya.99@gmail.com>
2019-04-06 23:10:19 +0600

    Change staticfiles var

--- buyhome/settings.py
+++ buyhome/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/admin')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/admin/')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'buyhome/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'buyhome/static')
+# ]
 
 # Media dir settings
 
